1403de7ef3812c316577f4d56b73dd10673a1227
prasad-gobburkar <prasadgobburkar@outlook.com>
2022-05-29 12:25:52 +0530

    Added Ingress Service

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,19 @@
+import { CustomError } from "./custom-error";
+
+export class DatabaseConnectionError extends CustomError {
+    statusCode = 500;
+    reason = 'Error connecting to database';
+    
+    constructor(){
+        super('Error Connecting to DB');
+
+
+        Object.setPrototypeOf(this, DatabaseConnectionError.prototype)
+    }
+
+    serializeErrors(){
+        return [
+            {message:this.reason}
+        ];
+    }
+}
