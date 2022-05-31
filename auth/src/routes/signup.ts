01c6b8596ed3ef70be858549e0a9d0d407cd5844
prasad-gobburkar <prasadgobburkar@outlook.com>
2022-05-31 14:13:00 +0530

    Auth Service Added

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,32 +1,54 @@
-import express, {Request, Response} from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
+import jwt from "jsonwebtoken";
+import { validateRequest } from "../models/validate-request";
 
 const router = express.Router();
 
-router.post('/api/users/signup', [
-    body('email')
-    .isEmail()
-    .withMessage('Email must be valid'),
-    body('password')
-    .trim()
-    .isLength({min: 4, max: 20})
-    .withMessage('Password must be between 4 and 20 characters')
-] ,(req: Request, res: Response) => {
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    
+    const { email, password } = req.body;
 
-    const errors = validationResult(req);
+    const existingUser = await User.findOne({ email });
 
-    if(!errors.isEmpty()){
-        throw new RequestValidationError(errors.array());
+    if (existingUser) {
+      // console.log('Email in use');
+      // return res.send({});
+      throw new BadRequestError("Email in use");
     }
 
+    const user = User.build({ email, password });
+    await user.save();
 
-   
+    //Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
 
-    console.log('Creating a user...')
-    throw new DatabaseConnectionError();
-    res.send({})
-});
+    //Store it on session object
 
-export {router as signupRouter};
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(201).send(user);
+  }
+);
+
+export { router as signupRouter };
